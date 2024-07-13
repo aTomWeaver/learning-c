691f83686a52cb80b25d018f459b4d0794cce40c
aTomWeaver <atomweavermail@gmail.com>
2024-07-13 18:07:47 -0400

    worked on linked lists

--- playground/linked_lists.c
+++ playground/linked_lists.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
-struct node NewNode(struct node lastNode, int val);
-
 struct node {
-    int val;
-    struct node* next;
+    int val;            // value of node
+    struct node* next;  // pointer to next node
 };
+typedef struct node node_t; // typedef for simplicity
+
+void print_vals_in_list(node_t *head);
 
 int main()
 {
-    struct node first = {1, NULL};
-    struct node nxt = NewNode(first, 24);
-    struct node last = NewNode(nxt, 48);
-    printf("%d\n", first.val);
-    printf("%d\n", nxt.val);
-    printf("%d\n", last.val);
+    node_t n1, n2, n3;
+    node_t *HEAD;
+
+    n1.val = 101;
+    n2.val = 202;
+    n3.val = 303;
+
+    HEAD = &n1;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = NULL;
+
+    print_vals_in_list(HEAD);
 
     return 0;
 }
 
-struct node NewNode(struct node lastNode, int val)
+/* assumes head node has no value */
+void print_vals_in_list(node_t *head)
 {
-    struct node newnode = {val, NULL};  // create new node with val
-    lastNode.next = &newnode;
-    return newnode;
+    node_t *cur;                            // create ptr for walking
+    cur = head;                             // set ptr to head node
+    printf("Value:\t%d\n", cur->val);       // print val of head node
+    while ((cur = cur->next) != NULL) {     // set ptr to addr of "next" node
+        printf("Value:\t%d\n", cur->val);   // print value of node
+    }
 }
